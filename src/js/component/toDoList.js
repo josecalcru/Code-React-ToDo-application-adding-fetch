f1e89c0454d821e63eb760858c28c48e7d3f22bf
josecalcru <josecalcru@gmail.com>
2021-03-06 05:51:33 +0000

    initial upload

---
+++ src/js/component/toDoList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+const todo = [1, 2, 3, 4, 5];
+const listItems = todo.map(todo => <li key={todo.toString()}>{todo}</li>);
+
+export const ToDoList = () => {
+	const [inputValue, setInputValue] = React.useState("");
+	const [list, setList] = React.useState([]);
+
+	return (
+		<div>
+			<input
+				type="text"
+				onKeyPress={e => {
+					if (e.key === "Enter") {
+						if (inputValue === "") {
+							alert("The input cannot be empty");
+						} else {
+							//alert(inputValue);
+							//list.push(
+							setList(list.concat(inputValue)); //usar concat
+						}
+					}
+				}}
+				onChange={e => setInputValue(e.target.value)}
+				value={inputValue}
+			/>
+			<ul className="list-group">
+				<li key={inputValue.toString()}>{inputValue}</li>
+			</ul>
+		</div>
+	);
+};
