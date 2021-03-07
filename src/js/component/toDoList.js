bc03adf091aec15ae343f89ef2cbe9d268ad7d44
josecalcru <josecalcru@gmail.com>
2021-03-07 07:42:18 +0000

    solution upload

--- src/js/component/toDoList.js
+++ src/js/component/toDoList.js
@@ -7,7 +7,7 @@ const todo = [1, 2, 3, 4, 5];
 const listItems = todo.map(todo => <li key={todo.toString()}>{todo}</li>);
 
 export const ToDoList = () => {
-	const [inputValue, setInputValue] = React.useState("");
+	var [inputValue, setInputValue] = React.useState("");
 	const [list, setList] = React.useState([]);
 
 	function handleRemove(task) {
@@ -17,36 +17,43 @@ export const ToDoList = () => {
 	}
 
 	return (
-		<div>
-			<input
-				type="text"
-				onKeyPress={e => {
-					if (e.key === "Enter") {
-						if (inputValue === "") {
-							alert("The input cannot be empty");
-						} else {
-							setList(list.concat(inputValue));
-						}
-					}
-				}}
-				onChange={e => setInputValue(e.target.value)}
-				value={inputValue}
-			/>
-			<ul className="list-group">
-				{list.map((item, index) => {
-					return (
-						<li key={item.toString()}>
-							{item}
-							<div className="icon ml-auto">
-								<i
-									className="fas fa-trash-alt"
-									onClick={() => handleRemove(item)}></i>
-							</div>
-						</li>
-					);
-				})}
-			</ul>
-			{list.length}
+		<div className="container d-flex align-items-center flex-column">
+			<div className="col-8 justify-content-center">
+				<h1 className="text-center">To Do:</h1>
+
+				<ul className="list-group text-center">
+					<input
+						type="text"
+						onKeyPress={e => {
+							if (e.key === "Enter") {
+								if (inputValue === "") {
+									alert("The input cannot be empty");
+								} else {
+									setList(list.concat(inputValue));
+									setInputValue((inputValue = ""));
+								}
+							}
+						}}
+						onChange={e => setInputValue(e.target.value)}
+						value={inputValue}
+					/>
+					{list.map((item, index) => {
+						return (
+							<li
+								className="list-group-item"
+								key={item.toString()}>
+								{item}
+								<div className="icon ml-auto">
+									<i
+										className="fas fa-trash-alt"
+										onClick={() => handleRemove(item)}></i>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+				<p>tasks to do: {list.length}</p>
+			</div>
 		</div>
 	);
 };
